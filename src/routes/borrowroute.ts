d8317cc94caa84b40990cd92f73e5a2c6e06cde5
unknown <bibektotol@gmail.com>
2025-07-23 16:08:35 +0600

    $$

--- src/routes/borrowroute.ts
+++ src/routes/borrowroute.ts
@@ -6,9 +6,13 @@ const router = Router();
 
 router.get("/borrowsummary", getBorrowSummary);
 router.post("/borrow", createBorrow);
-router.delete("/borrow/:id", deleteBorrow);
+router.delete("/borrow/:id", async (req, res) => {
+    await deleteBorrow(req, res);
+  });
 
-router.patch("/edit-borrowsummary/:id", editBorrow);
+router.patch("/edit-borrowsummary/:id", async(req, res) => {
+    await editBorrow(req, res);
+});
 
 export default router;
 
--- src/routes/librarydataroute.ts
+++ src/routes/librarydataroute.ts
@@ -5,7 +5,11 @@ const router = Router();
 
 router.get("/books", getAllBooks);
 router.post("/books", createBook);
-router.delete("/books/:id", deleteBook);
-router.patch("/edit-books/:id", editBooks);
+router.delete("/books/:id", async (req, res) => {
+    await  deleteBook(req, res);
+});
+router.patch("/edit-books/:id", async(req, res) => {
+    await  editBooks(req, res);
+});
 
 export default router;
